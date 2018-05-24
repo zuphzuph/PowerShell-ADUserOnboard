02cc35908a48de1d0a5f35e080d2f4eafce4492c
zuphzuph <zuphzuph@users.noreply.github.com>
2018-05-23 20:09:36 -0600

    Commited.
    yeeeet

---
+++ Onboard-User.ps1
@@ -0,0 +1,25 @@
+﻿$username = "luser"
+$firstname = "l"
+$lastname = "user"
+$domain = "@email.com"
+$office = "Office/Branch Location"
+$jobtitle = "Test Dummy"
+$telephonenumber = "(XXX)XXX-XXXX"
+$description = "Desc for Account in AD"
+$logonscript = "Runs upon login, often inclues mapped shares"
+$homedriveletter = "DriveLetter"
+$homedirpath = "\\pathtohome\share"
+
+New-ADUser -Name "$username" -GivenName "$firstname" -Surname "$lastname" `
+-Path "OU=end,OU=to,OU=start,DC=www,DC=google,DC=com" `
+-EmailAddress ("$username" + "$domain") `
+-OfficePhone ($telephonenumber) `
+-AccountPassword (Read-Host -AsSecureString "AccountPassword") `
+-Office ($office) `
+-Title ($jobtitle) `
+-Description ($description) `
+-ScriptPath ($logonscript) `
+-HomeDrive ($homedriveletter) `
+-HomeDirectory ("$homedirpath" + "$username") `
+-UserPrincipalName ("$username" + "$domain") `
+-PassThru | Enable-ADAccount `
